a74acba1febf240ba8b58d4ceb8f436beb0e050b
Mateo Bodini <laseis123@gmail.com>
2023-04-11 19:37:46 -0300

    SuccessMessageAdd

--- public/app.js
+++ public/app.js
@@ -3,13 +3,30 @@ const input = document.querySelector("#email__input");
 const error = document.querySelector(".p-active");
 const emailReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+
+// show message
+
+const showSuccessMessage = () => {
+  let message = document.createElement('div');
+  message.classList.add("message")
+  message.innerText = 'Thanks for subscribing!'
+  document.body.appendChild(message);
+
+  setTimeout(() => {
+    document.body.removeChild(message);
+  }, 3000);
+}
+
+
 btn.addEventListener("click", () => {
   let mail = input.value;
   if (emailReg.test(mail)) {
     error.classList.remove("error")
     error.classList.add("p-active")
+    showSuccessMessage();
   } else {
     error.classList.remove("p-active")
     error.classList.add("error")
   }
 });
+
